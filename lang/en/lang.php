1d13c071aacabffbb265cac1549ce37836988f2d
Klap-in <klapinklapin@gmail.com>
2013-01-17 11:25:36 +0100

    Added Dutch, update other languages.

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -3,6 +3,6 @@
  * english language file
  */
 
-$lang['fig']           = 'Fig. ';
+$lang['fig']           = 'Fig.';
 $lang['figure']        = 'figure';
 $lang['error_imgrefbeforeimgcaption']      = 'imgref tag is placed before the referenced imgcaption tag. This is not allowed';
---
+++ lang/nl/lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * dutch language file
+ */
+
+$lang['fig']           = 'Fig.';
+$lang['figure']        = 'figuur';
+$lang['error_imgrefbeforeimgcaption'] = 'imgref tag is voor de imgcaption tag geplaatst waarnaar die verwijst. Dit is niet mogelijk.';
